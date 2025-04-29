584e2e768c5ef30d695f563de026dfe852bca0f5
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-04-29 09:44:20 +0800

    Added header and navbar

--- home/home.php
+++ home/home.php
@@ -1,26 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/styles.css">
     <link rel="stylesheet" href="../styles/home.styles.css">
-    <link rel="icon" href="../assets/Logo.png">
+    <link rel="icon" href="../assets/icon/#Circle_Logo.png">
     <title>Hanzo's Cakery</title>
 </head>
+
 <body>
     <header>
         <div class="header-container">
-            <img src="../assets/Logo.svg" alt="Hanzo's Bakery Logo" class="logo">
+            <img src="../assets/icon/Logo_White.svg" alt="Hanzo's Bakery Logo" class="logo">
             <h1>Hanzo's Bakery</h1>
         </div>
+        <div class="navbar">
+            <form class="search-container" action="" method="GET">
+                    <input type="text" placeholder="Search">
+                    <a href=""><img src="../assets/icon/search.svg" alt=""></a>
+            </form>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="24" height="24">
+                                <path d="M22.713,4.077A2.993,2.993,0,0,0,20.41,3H4.242L4.2,2.649A3,3,0,0,0,1.222,0H1A1,1,0,0,0,1,2h.222a1,1,0,0,1,.993.883l1.376,11.7A5,5,0,0,0,8.557,19H19a1,1,0,0,0,0-2H8.557a3,3,0,0,1-2.82-2h11.92a5,5,0,0,0,4.921-4.113l.785-4.354A2.994,2.994,0,0,0,22.713,4.077ZM21.4,6.178l-.786,4.354A3,3,0,0,1,17.657,13H5.419L4.478,5H20.41A1,1,0,0,1,21.4,6.178Z" />
+                                <circle cx="7" cy="22" r="2" />
+                                <circle cx="17" cy="22" r="2" />
+                            </svg>
+                        </a>
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24">
+                                <path d="M16.043,14H7.957A4.963,4.963,0,0,0,3,18.957V24H21V18.957A4.963,4.963,0,0,0,16.043,14Z" />
+                                <circle cx="12" cy="6" r="6" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="nav-container">
         <nav>
             <ul>
                 <li><a href="#" class="active">Home</a></li>
-                <li><a href="#">Cart</a></li>
-                <li><a href="#">Profile</a></li>
+                <li><a href="#">Cakes</a></li>
+                <li><a href="#">Contact Us</a></li>
             </ul>
         </nav>
-    </header>
+    </div>
+    <main>
+        <section class="hero-section">
+            <div class="hero-left">
+                <h2>Welcome to Hanzo's Bakery</h2>
+                <p>Your one-stop shop for delicious cakes and pastries.</p>
+                <button>Order Now</button>
+            </div>
+            <div class="hero-right">
+                <img src="../assets/4.jpg" alt="">
+            </div>
+        </section>
+    </main>
 </body>
+
 </html>
